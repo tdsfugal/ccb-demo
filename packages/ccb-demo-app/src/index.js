77c296d1648504d7c04be255fa33797a47ee6458
TDSFugal <tim.smith@fugaltech.com>
2018-06-16 13:15:22 -0400

    Snapshot save before inmporting CCB to monorepo

--- packages/ccb-demo-app/src/index.js
+++ packages/ccb-demo-app/src/index.js
@@ -3,10 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
+import { encrypt, decrypt, setupCrypto } from 'ccb-demo-crypto';
+import { setupCCB } from 'crypto-collaboration-barrier';
 
 import App from './app';
 
 import './index.css';
+
+setupCrypto({ passkey: 'abc123' });
+setupCCB({ encrypt, decrypt });
 
 const client = new ApolloClient({
   uri: 'http://localhost:4002/graphql',
--- packages/ccb-demo-crypto/src/index.js
+++ packages/ccb-demo-crypto/src/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-confusing-arrow */
-const encryptString = text =>
+const encrypt = text =>
   typeof text === 'string' || text instanceof String
     ? `ENCRYPTED - ${text}`
     : null;
 
-const decryptString = hash =>
+const decrypt = hash =>
   typeof hash === 'string' || hash instanceof String ? hash.slice(12) : null;
 
-export { encryptString, decryptString };
+const setupCrypto = options => console.log(options);
+
+export { encrypt, decrypt, setupCrypto };
