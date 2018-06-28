7b9206d1a34f7802391b27100247bca676d390d3
TDSVirtru <tsmith@virtru.com>
2018-06-28 11:44:14 -0400

    snapshot

--- packages/ccb-demo-server/src/db/index.js
+++ packages/ccb-demo-server/src/db/index.js
@@ -3,7 +3,7 @@ import uuid from 'uuid';
 import booksClearText from './booksClearText';
 import { encryptString } from './crypto';
 
-const ENCRYPT_REVIEW = false;
+const ENCRYPT_REVIEW = true;
 
 const db = booksClearText.reduce((acc, book) => {
   acc[uuid()] = ENCRYPT_REVIEW ? { ...book, review: encryptString(book.review) } : book;
